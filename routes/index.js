dabd8a040d9a23a05edf5cb9cef758627e6f2c60
kthomas <kevin.allan.thomas@gmail.com>
2021-11-20 17:18:32 +0100

    Iteration 1 in progress

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+const salt = bcrypt.genSaltSync(saltRounds);
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/registration", (req, res, next) => {
+  res.render("registration");
+});
+
+router.post("/registration", (req, res, next) => {
+  const {Username, Password} = req.body; 
+  const hash = bcrypt.hashSync(Password, salt);
+  User.create({
+    Username,
+    passwordHash: hash
+  })
+  .then(createUser => {
+    res.redirect("/")
+  })
+  .catch(error => next(error));
 });
 
 module.exports = router;
