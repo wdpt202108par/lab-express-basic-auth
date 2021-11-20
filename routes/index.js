e1816fe6b9c9eb34499c285d467a9631f5711e80
hassan sabeh <h.adamsabeh@yahoo.com>
2021-11-20 17:16:15 +0100

    iteration 1 done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('auth/signUp');
+});
+
+router.post('/signup', function(req, res, next) {
+  const {username, password} = req.body;
+  User.create({username, password})
+    .then(userCreated => console.log("user created", userCreated))
+    .catch(err => next(err))
+})
 
 module.exports = router;
