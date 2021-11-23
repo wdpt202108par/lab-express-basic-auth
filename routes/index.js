d18f59c15f3a8ef5bbb66a5d6b3d0c017ebbdbff
Cindy <cindy.confiant@gmail.com>
2021-11-23 16:42:39 +0100

    iteration 1 password encrypted

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+const salt = bcryptjs.genSaltSync(saltRounds)
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+// ITERATION 1 : sign up
 router.get('/signup', function(req, res, next) {
   res.render('auth/signUp');
 });
 
 router.post('/signup', function(req, res, next) {
   const {username, password} = req.body;
-  User.create({username, password})
+
+  const hashedPassword = bcryptjs.hashSync(password, salt);
+
+  User.create({username, password: hashedPassword})
     .then(userCreated => console.log("user created", userCreated))
     .catch(err => next(err))
 })
