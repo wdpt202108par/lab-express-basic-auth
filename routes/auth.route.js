4c23024635f0ac3830e5ef7d8509d8fbbb14935c
Claire Pottier <claire_pottier@hotmail.fr>
2021-11-23 21:58:37 +0100

    Iteration 1 done

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,23 @@
 const { Router } = require('express');
 const router = new Router();
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User.model');
 
-router.get('/signup', (req, res) => res.render('auth/signup'))
+router.get('/signup', (req, res) => res.render('signup'))
 
 router.post('/signup', (req, res, next) => {
-    console.log ('The form data: ', req.body)
+    const salt = bcryptjs.genSaltSync(10);
+    const encryptedPassword = bcryptjs.hashSync(req.body.password, salt);
+
+    User.create({
+        username: req.body.username,
+        passwordHash: encryptedPassword
+    })
+        .then(function (req, res, next) {
+            res.send('Utilisateur créé')
+            //res.redirect('/profile')
+        })
+        .catch(err => next(err))
 })
 
 module.exports = router;
