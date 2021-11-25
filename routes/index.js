ae70e438bb80722012c8546a82ba79ce08791c24
Cindy <cindy.confiant@gmail.com>
2021-11-25 23:57:25 +0100

    bonus WIP

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const bcryptjs = require('bcryptjs');
+const { Mongoose } = require("mongoose");
 const saltRounds = 10;
 const salt = bcryptjs.genSaltSync(saltRounds)
 
@@ -16,23 +17,39 @@ router.get('/signup', function(req, res, next) {
 
 router.post('/signup', function(req, res, next) {
   const {username, password} = req.body;
+  // BONUS : no empty field
+  if(!username || !password) {
+    res.render("auth/signUp", {
+      errMessage: "Username or password should not be empty"
+    });
+    return;
+  }
 
   const hashedPassword = bcryptjs.hashSync(password, salt);
 
   User.create({username, password: hashedPassword})
     .then(userCreated => res.render("myProfile", {
-      username: userCreated.username} ))
-    .catch(err => next(err))
+      username: userCreated.username} 
+    ))
+    .catch(err => {
+      // BONUS : no repeated username
+      if(err.code === 11000) {
+        res.status(500).render("auth/signUp", {errMessage: "Username already used. Please choose another one."});
+      } else {
+        next(err);
+      }
+    })
 })
-
+  
+// ITERATION 2 : login
 router.get("/login", function(req, res, next) {
   res.render("auth/login", {})
 })
 
 router.post("/login", function(req, res, next) {
   const {username, password} = req.body
-  //validation
-  if(!username || !password) { //=> empty
+  //BONUS : no empty field
+  if(!username || !password) { 
     res.render("auth/login", {
       errMessage: "Username or password should not be empty"
     })
@@ -41,44 +58,50 @@ router.post("/login", function(req, res, next) {
   //query db
   User.findOne({username: username})
     .then(function(userFromDb) {
+      //BONUS : no invalid fields
       if(!userFromDb) {
         res.render("auth/login", {errMessage: "user not registered"})
         return;
       } else if (bcryptjs.compareSync(password, userFromDb.password)) {
         console.log('SESSION:', req.session);
         req.session.user = userFromDb.username;
-        res.render("myProfile", {username: userFromDb.username});
+        res.redirect('/myProfile')
       } else {
         res.render("auth/login", {errMessage: "invalid username/password"});
       }
     })
     .catch(err => console.log(err))
-    //check if username and password are valid
-    // render vers la page profile
-
 })
 
+
+router.get('/myProfile', function (req, res, next) {
+  if (!req.session.user) return res.redirect('/')
+
+  const username = req.session.user
+  res.render("myProfile", {username: username});
+})
+
+
+// ITERATION 3 : Protected Routes
 router.get("/main", function(req,res,next) {
   if(!req.session.user){
-    res.render("auth/login", {errMessage: "login to view this page"})
+    res.redirect('/');
+    return;
+  } else {
+  res.render("main", {});
   }
-  res.render("main");
 })
-
 router.get("/private", function(req,res,next) {
   if(!req.session.user){
-    res.render("auth/login", {errMessage: "login to view this page"})
+    res.redirect('/')
   }
-  res.render("private");
+  res.render("private", {});
 })
 
+// Log out
 router.get("/logout", function(req, res, next) {
   req.session.destroy()
   res.render("auth/login", {})
 })
-
-// router.get("/myprofile/:name", function(req, res, next) {
-//   res.render("myProfile", {username: req.params.name})
-// })
 
 module.exports = router;
